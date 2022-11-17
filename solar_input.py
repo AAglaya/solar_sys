2cd9a2daf215531d9c817ffd647e991500f74d42
Aglaya <aglaya.ashurkova@gmail.com>
2022-11-17 23:05:22 +0300

    parse_star_parameters and parse_planet_parameters fixed

--- solar_input.py
+++ solar_input.py
@@ -21,12 +21,12 @@ def read_space_objects_data_from_file(input_filename):
 
             object_type = line.split()[0].lower()
             if object_type == "star":
-                star = Star()
-                parse_star_parameters(line, star)
+                star_parameters = parse_star_parameters(line)
+                star = Star(**star_parameters)
                 objects.append(star)
             elif object_type == "planet":
-                planet = Planet()
-                parse_planet_parameters(line, planet)
+                planet_parameters = parse_planet_parameters(line)
+                planet = Planet(**planet_parameters)
                 objects.append(planet)
             else:
                 print("Unknown space object")
@@ -34,7 +34,7 @@ def read_space_objects_data_from_file(input_filename):
     return [DrawableObject(obj) for obj in objects]
 
 
-def parse_star_parameters(line, star):
+def parse_star_parameters(line):
     """Считывает данные о звезде из строки.
 
     Входная строка должна иметь слеюущий формат:
@@ -53,9 +53,14 @@ def parse_star_parameters(line, star):
 
     **star** — объект звезды.
     """
-    pass  # FIXME: допишите парсер
 
-def parse_planet_parameters(line, planet):
+    line_to_list = list(line.split())
+    parameters = {'R': int(line_to_list[1]), 'color': str(line_to_list[2]), 'm': float(line_to_list[3]),
+                  'x': float(line_to_list[4]), 'y': float(line_to_list[5]), 'Vx': float(line_to_list[6]),
+                  'Vy': float(line_to_list[7])}
+    return parameters
+
+def parse_planet_parameters(line):
     """Считывает данные о планете из строки.
     Входная строка должна иметь слеюущий формат:
 
@@ -73,7 +78,11 @@ def parse_planet_parameters(line, planet):
 
     **planet** — объект планеты.
     """
-    pass  # FIXME: допишите парсер
+    line_to_list = list(line.split())
+    parameters = {'R': int(line_to_list[1]), 'color': str(line_to_list[2]), 'm': float(line_to_list[3]),
+                  'x': float(line_to_list[4]), 'y': float(line_to_list[5]), 'Vx': float(line_to_list[6]),
+                  'Vy': float(line_to_list[7])}
+    return parameters
 
 def write_space_objects_data_to_file(output_filename, space_objects):
     """Сохраняет данные о космических объектах в файл.
